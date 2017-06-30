25499dfdbba38841b0947241f00ca300cef408ae
Krister Löfgren <krr@devio.us>
2017-06-30 18:53:24 +0200

    Erosion.sh

---
+++ colorschemes/erosion.sh
@@ -0,0 +1,25 @@
+# erosion
+
+background="#181512"
+foreground="#BEA492"
+cursor="#93A1A1"
+mouse_background=""
+mouse_foreground=""
+highlight=""
+border="#181512"
+color0="#332D29" # black
+color1="#8C644C" # red
+color2="#C4BE90" # green
+color3="#BFBA92" # yellow
+color4="#293033" # blue
+color5="#6D6871" # magenta
+color6="#3B484A" # cyan
+color7="#504339" # white
+color8="#817267" # brblack
+color9="#9F7155" # brred
+color10="#BEC17E" # brgreen
+color11="#FAFAC0" # bryellow
+color12="#646A6D" # brblue
+color13="#756F7B" # brmagenta
+color14="#444D4E" # brcyan
+color15="#9A875F" # brwhite
