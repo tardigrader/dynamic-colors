3b910acc9a2aa4f6973385b00d1c8a009a2b00ce
Krister Löfgren <krr@devio.us>
2017-06-30 18:54:21 +0200

    Tango colorscheme.

---
+++ colorschemes/tango.sh
@@ -0,0 +1,25 @@
+# tango
+
+background="#000000"
+foreground="#FFFFFF"
+cursor=""
+mouse_background=""
+mouse_foreground=""
+highlight=""
+border="#000000"
+color0="#2E3436" # black
+color1="#A40000" # red
+color2="#4E9A06" # green
+color3="#C4A000" # yellow
+color4="#3465A4" # blue
+color5="#75507B" # magenta
+color6="#CE5C00" # cyan
+color7="#BABDB9" # white
+color8="#555753" # brblack
+color9="#EF2929" # brred
+color10="#8AE234" # brgreen
+color11="#FCE94F" # bryellow
+color12="#729FCF" # brblue
+color13="#AD7FA8" # brmagenta
+color14="#FCAF3E" # brcyan
+color15="#EEEEEC" # brwhite
