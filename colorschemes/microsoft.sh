409bfff98106f438db0ed2b37b3ee42498e0c01e
Krister Löfgren <krr@devio.us>
2017-06-30 18:53:45 +0200

    Powershell

---
+++ colorschemes/microsoft.sh
@@ -0,0 +1,25 @@
+# microsoft
+
+background="#012456"
+foreground="#EEEDF0"
+cursor="#4C6EFF"
+mouse_background=""
+mouse_foreground=""
+highlight=""
+border="#012456"
+color0="#000000" # black
+color1="#FFCC2B" # red
+color2="#D9D4FF" # green
+color3="#262626" # yellow
+color4="#00A7FF" # blue
+color5="#AD72FF" # magenta
+color6="#00475D" # cyan
+color7="#FAEBD7" # white
+color8="#404040" # brblack
+color9="#FFFF8C" # brred
+color10="#FCFF7F" # brgreen
+color11="#00062D" # bryellow
+color12="#7BD1FF" # brblue
+color13="#FF00FF" # brmagenta
+color14="#00475D" # brcyan
+color15="#FFFFFF" # brwhite
