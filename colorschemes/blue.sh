525fb649682e26aa48e7437afc285d014d7cf548
Krister Löfgren <krr@devio.us>
2017-06-30 18:51:12 +0200

    Blue-ish

---
+++ colorschemes/blue.sh
@@ -0,0 +1,25 @@
+# blue
+
+background="#1b1b1b"
+foreground="#FFFFFF"
+cursor="#FFFFFF"
+mouse_background=""
+mouse_foreground=""
+highlight=""
+border="#1b1b1b"
+color0="#3D3D3D" # black
+color1="#6673BF" # red
+color2="#3EA290" # green
+color3="#B0EAD9" # yellow
+color4="#31658C" # blue
+color5="#596196" # magenta
+color6="#8292B2" # cyan
+color7="#C8CACC" # white
+color8="#4D4D4D" # brblack
+color9="#899AFF" # brred
+color10="#52AD91" # brgreen
+color11="#98C9BB" # bryellow
+color12="#477AB3" # brblue
+color13="#7882BF" # brmagenta
+color14="#95A7CC" # brcyan
+color15="#EDEFF2" # brwhite
