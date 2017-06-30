b446070f38b75114101531d44661e9e5c55d1f0a
Krister Löfgren <krr@devio.us>
2017-06-30 18:52:27 +0200

    "Earthy", brown and green and... earthy colors

---
+++ colorschemes/earthy.sh
@@ -0,0 +1,25 @@
+# "Earthy"
+
+background="#121212"
+foreground="#ACACAC"
+cursor="#25383C"
+mouse_background=""
+mouse_foreground=""
+highlight=""
+border="#121212"
+color0="#2B1D1D" # black
+color1="#45352E" # red
+color2="#5E513F" # green
+color3="#787250" # yellow
+color4="#8b9161" # blue
+color5="#93AB72" # magenta
+color6="#96C484" # cyan
+color7="#96DE95" # white
+color8="#5E4949" # brblack
+color9="#78685D" # brred
+color10="#918370" # brgreen
+color11="#ABA584" # bryellow
+color12="#BFC497" # brblue
+color13="#C9DEAB" # brmagenta
+color14="#A5C497" # brcyan
+color15="#ACDEAB" # brwhite
